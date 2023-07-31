2625d52021729e93618e6d4de7f1a1fc09e819cb
Alan Cleyton <alancleyton67@gmail.com>
2023-07-31 19:02:24 -0300

    clean project base

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
+  semi: true,
   trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  endOfLine: 'lf'
 };
